beb14eaaf4ed34140f58d8c9e2208c3d56f23281
Elizabeth Rubio <ecrubio@miners.utep.edu>
2020-03-21 22:43:00 -0600

    Two directories were made along with filling timeLib and creating a main file

---
+++ project/README.md
@@ -0,0 +1,6 @@
+- Directory src contains most of the code that controls the msp430
+- Directory timeLib contains the basic code to make the msp430 keep track of
+ time 
+__________________
+
+What it should do:
---
+++ project/src/main.c
@@ -0,0 +1,16 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "switches.h"
+#include "led.h"
+#include "buzzer.h"
+
+int main(void) {
+  
+  configureClocks();/* setup master oscillator, CPU & peripheral clocks(timerLib executed) */
+  led_init();
+  switch_init();
+  buzzer_init();
+  enableWDTInterrupts();/* enable periodic interrupt */
+
+  or_sr(0x18);/* CPU off, GIE on */
+}
